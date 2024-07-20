0bbf67a698ea006c24d1d0c04fe36230be70423d
Frooge <jaantaros@gmail.com>
2024-07-20 12:23:32 +0800

    initialize repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <main className='main'>
+      <div className='container'>
+
+      </div>
+    </main>
+  )
+}
+
+export default App
